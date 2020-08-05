66ab97d248aa059bb0dc2f586652d12071e0aa2f
Konrad-Konrad <67336690+Konrad-Konrad@users.noreply.github.com>
2020-08-05 15:46:57 +0200

    Updates

---
+++ precache-manifest.4891b57e415e581d8f7e63be6a936820.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57df2ad006721ce11a152a408359d111",
+    "url": "/currencyConverterReact.js/index.html"
+  },
+  {
+    "revision": "8df1fe3ee420ccfa183b",
+    "url": "/currencyConverterReact.js/static/css/main.1d374679.chunk.css"
+  },
+  {
+    "revision": "a157860b9200b3aff2ce",
+    "url": "/currencyConverterReact.js/static/js/2.fa44e786.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currencyConverterReact.js/static/js/2.fa44e786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df1fe3ee420ccfa183b",
+    "url": "/currencyConverterReact.js/static/js/main.b2977813.chunk.js"
+  },
+  {
+    "revision": "edf81c4da8ff46a2eca1",
+    "url": "/currencyConverterReact.js/static/js/runtime-main.860496c9.js"
+  },
+  {
+    "revision": "6edcff739ac95d66a4c24607c74e34f2",
+    "url": "/currencyConverterReact.js/static/media/background.6edcff73.jpg"
+  }
+]);
